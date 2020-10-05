e991edd4f89801ca34c8f1700b19a5beb92d1130
graham_bates <graham.bates@spglobal.com>
2020-10-05 11:13:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,74 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, {Fragment, useState} from 'react';
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost';
+
+import { ApolloProvider } from "react-apollo";
+
+import { Query } from "react-apollo";
+
+import gql from "graphql-tag";
+
+const GETCARD = gql`
+                query {
+   
+                    page {
+                      user_id
+                      layout
+                      timestamp
+                      layout 
+                      cards {
+                        card {
+                          id
+                          html
+                          type
+                          camera
+                        }
+                      }
+                        
+                      
+                    }
+                    
+                  }
+                
+
+`
+
+export default class WebMapView extends React.Component {
+
+    state = { selectedCardSet : null };
+
+    httpLink = new HttpLink({ uri: 'https://beatroute2019.herokuapp.com/v1/graphql' });
+
+    client = new ApolloClient({ link: (this.httpLink), cache: new InMemoryCache() });
+
+    render () {
+
+        //return <Editor/>
+
+        const data = {"page":[{"user_id":123,"layout":"WoodFrame","timestamp":"2020-04-01T00:00:00+00:00","cards":[{"card":{"id":100,"html":"Northumbria 2021","type":"FrontCover","camera":{"x":2553700.249376615,"y":-1.115334886935115e-9,"z":9000673.36695155},"__typename":"cards"},"__typename":"card_positions"},{"card":{"id":97,"html":"Week 1","type":"Chapter","camera":{"x":4553700.249376615,"y":-1.115334886935115e-9,"z":9000673.36695155},"__typename":"cards"},"__typename":"card_positions"},{"card":{"id":101,"html":"","type":"PhotoCard","camera":null,"__typename":"cards"},"__typename":"card_positions"}],"__typename":"page"}]}
+
+       // return <ScrollTrigger cards={ data.page[0].cards.map(d => d.card)} />
+
+
+
+        if (true) return <Fragment>
+
+            <ApolloProvider client={this.client}>
+                {/*<ScrollTrigger cards={ [{id : '1' , type : 'FrontCover'}, {id : '2' , type : 'FrontCover'}] } />*/}
+                <Query query={GETCARD}  >
+                    {({ loading, error, data, refetch  }) => {
+
+                        if (loading || !data) return <span>loading</span>
+
+                        return <span>{JSON.stringify(data)}</span>
+
+                    }}
+
+                </Query>
+
+            </ApolloProvider>
+
+        </Fragment>
+
     }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
 }
-
-export default App;
